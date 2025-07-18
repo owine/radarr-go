cc00f90462411b1f446a034000b35d54c2ec9405
owine <owine@users.noreply.github.com>
2025-07-18 13:50:50 -0500

    cleanup: remove pre-commit test file

--- test_precommit.go
+++
@@ -1,9 +0,0 @@
-// Package main provides a test for pre-commit hooks functionality.
-package main
-
-import "fmt"
-
-// main demonstrates that pre-commit hooks are working correctly.
-func main() {
-	fmt.Println("Testing pre-commit hooks")
-}
